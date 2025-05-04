339af1340be10cbec33e6d4b19a29d79c9719f96
SimeonNedyalkov <simeon_sg@abv.bg>
2025-05-04 19:22:11 +0300

    added 3d project

--- simeonnedyalkov/src/components/Projects.tsx
+++ simeonnedyalkov/src/components/Projects.tsx
@@ -1,3 +1,20 @@
+import { Canvas } from "@react-three/fiber";
+import { CatDate } from "./ThreedComponents/CatDate";
+import * as THREE from "three";
+import { Suspense } from "react";
+import { OrbitControls, Stage } from "@react-three/drei";
 export default function Projects() {
-  return <div>Hello!</div>;
+  return (
+    <div>
+      <div className="projectsContainer">Hello!</div>
+      <Canvas>
+        <Suspense fallback="...loading">
+          <Stage environment="city">
+            <CatDate />
+          </Stage>
+          <OrbitControls enableZoom={false}></OrbitControls>
+        </Suspense>
+      </Canvas>
+    </div>
+  );
 }

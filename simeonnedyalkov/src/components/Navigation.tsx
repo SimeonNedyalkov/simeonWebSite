0bc3431c26fe785c8e4d115d7eee3e754b101f97
SimeonNedyalkov <simeon_sg@abv.bg>
2025-05-07 15:34:32 +0300

    fix for mobile

--- simeonnedyalkov/src/components/Navigation.tsx
+++ simeonnedyalkov/src/components/Navigation.tsx
@@ -1,9 +1,55 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 export default function Navigation() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <nav className="navigationContainer">
       <div className="navigationLogo">
         Simeon<span className="navigationLogo2">.</span>
+      </div>
+      {/* menu for phone */}
+      <div className="hamburger">
+        <svg
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          stroke="#ffffff"
+          width="24px"
+          height="24px"
+        >
+          <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+          <g
+            id="SVGRepo_tracerCarrier"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          ></g>
+          <g id="SVGRepo_iconCarrier">
+            {" "}
+            <path
+              d="M4 7L7 7M20 7L11 7"
+              stroke="#ffffff"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+            ></path>{" "}
+            <path
+              d="M20 17H17M4 17L13 17"
+              stroke="#ffffff"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+            ></path>{" "}
+            <path
+              d="M4 12H7L20 12"
+              stroke="#ffffff"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+            ></path>{" "}
+          </g>
+        </svg>
       </div>
       <div className="navigationItems">
         <NavLink
